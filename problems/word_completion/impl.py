2976a20491ff812e579163e70ffb86568b612f7e
noec <noec@ethz.ch>
2025-12-08 09:57:11 +0100

    Added the implementation for  suggestionwordstrie which actually works

--- problems/word_completion/impl.py
+++ problems/word_completion/impl.py
@@ -80,8 +80,88 @@ class Solution1:
                     return_suggested_words.append([])
                 break
 
-        return return_suggested_words 
+        return return_suggested_words
+
+class node_str:
+    def __init__(self, stringValue: str|None = None, endOfWord: bool = False):
+        self.stringValue = stringValue
+        self.endOfWord = endOfWord
+        self.nextNodes = []
+
+class suggestion_word_trie:
+    def __init__(self):
+        self.root = node_str()
+    
+    def addWord(self, wordToAdd: str):
+        currNode = self.root
+        for idx, charWord in enumerate(wordToAdd):
+            currValue = wordToAdd[:(idx+1)]
+            nextStringsList = [n.stringValue for n in currNode.nextNodes]
+            if currValue not in nextStringsList:
+                newNode = node_str(currValue)
+                currNode.nextNodes.append(newNode)
+                currNode = newNode
+            else:
+                currNode = currNode.nextNodes[nextStringsList.index(currValue)]
+        
+        # make sure that last node has eow=true
+        currNode.endOfWord = True
+    
+    # Order is useless if the products are put in in lexicongrafic order
+    def order(self, node: node_str):
+        interNode = node
+        iterNode.nextNodes.sort(key = lambda n: n.stringValue)
+        for n in iterNode.nextNodes:
+            self.order(n)
+
+    def find_three_words(self, node: node_str) -> List[str]:
+        wordList = []
+        iterNode = node
+        if node.endOfWord:
+            wordList.append(node.stringValue)
+        
+        for n in iterNode.nextNodes:
+            subWords = self.find_three_words(n)
+            for subWord in subWords:
+                wordList.append(subWord)
+                if len(wordList) >= 3:
+                    return wordList
+
+        return wordList
+    
+    # Takes a from_node and a value and returns the node of the next.list 
+    # with that value
+    def return_node_from_node_with_val(self, from_node: node_str,  value: str) -> node_str:
+        for to_node in from_node.nextNodes:
+            if to_node.stringValue == value:
+                return to_node
+        return None
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
+        trie = suggestion_word_trie()
+
+        # Add all the words to the trie
+        for product in products:
+            trie.addWord(product)
+
+        # Go through the trie, at each node (or tipped character)
+        # suggest 3 possible completions
+        # if no completion possible return []
+        node_iter = trie.root
+        suggested_word_per_char = [[] for _ in range(len(searchWord))]
+        for idx, charWord in enumerate(searchWord):
+            currValue = searchWord[:(idx+1)]
+            node_iter = trie.return_node_from_node_with_val(node_iter, currValue)
+            if node_iter is not None:
+                suggested_word_per_char[idx] = trie.find_three_words(node_iter)
+            else:
+                return suggested_word_per_char
+        
+        return suggested_word_per_char
 
 SOLUTIONS = {
     "solution from hell": lambda args: Solution1.suggestedProducts(*args),
+    "optimized solution": lambda args: Solution().suggestedProducts(*args),
 }
